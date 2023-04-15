80cadc8531fb91a08ffde20ea6167d368a6b0018
LeeJunGyun <rainmaker0303@hanmail.net>
2023-04-15 15:45:20 +0900

    feat: 재고 관리 로직 정상 동작하도록 수정

--- core/core-api/src/main/kotlin/io/simple/productOrder/infrastructure/product/ProductExecutorImpl.kt
+++ core/core-api/src/main/kotlin/io/simple/productOrder/infrastructure/product/ProductExecutorImpl.kt
@@ -1,31 +1,35 @@
 package io.simple.productOrder.infrastructure.product
 
+import io.simple.productOrder.domain.product.Product
 import io.simple.productOrder.domain.product.ProductCommand
 import io.simple.productOrder.domain.product.ProductExecutor
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
 @Service
 class ProductExecutorImpl(private val productRepository: ProductRepository) : ProductExecutor {
-
-    private val log = LoggerFactory.getLogger(javaClass)
-
-    override fun reduceStock(reduceProduct: ProductCommand.ReduceProduct) {
-        log.info("reduceStock... {}", reduceProduct)
-
-        productRepository.findById(reduceProduct.getProductId())
+    override fun reduceStock(reduceProduct: ProductCommand.ReduceProduct): Mono<Product> {
+        return productRepository.findById(reduceProduct.getProductId())
             .switchIfEmpty(Mono.error(IllegalArgumentException("Product not found: ${reduceProduct.getProductId()}")))
             .flatMap { product ->
-                if (product.getStock() < reduceProduct.getQuantity()) {
-                    Mono.error(IllegalArgumentException("Insufficient stock for product ${product.getId()}"))
-                } else {
-                    productRepository.reduceStock(product.getId(), reduceProduct.getQuantity())
-                        .filter { rowsUpdated -> rowsUpdated == 1 }
-                        .switchIfEmpty(Mono.error(IllegalStateException("Failed to reduce stock for product ${product.getId()}")))
-                        .thenReturn(product)
-                }
+                validateStock(product, reduceProduct.getQuantity())
+                    .flatMap {
+                        productRepository.reduceStock(product.getId(), reduceProduct.getQuantity())
+                            .filter { rowsUpdated -> rowsUpdated == 1 }
+                            .switchIfEmpty(Mono.error(IllegalStateException("Failed to reduce stock for product ${product.getId()}")))
+                            .thenReturn(product)
+                    }
             }
-            .subscribe()
+            .onErrorResume { e ->
+                // 예외 처리 로직
+                Mono.error(e)
+            }
+    }
+
+    private fun validateStock(product: Product, quantity: Int): Mono<Product> {
+        if (product.getStock() < quantity) {
+            return Mono.error(IllegalArgumentException("Insufficient stock for product ${product.getId()}"))
+        }
+        return Mono.just(product)
     }
 }
