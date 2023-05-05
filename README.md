4a6a4d252488d8dce89937de9a8c2db11725e430
LeeJunGyun <rainmaker0303@hanmail.net>
2023-05-05 23:43:32 +0900

    분산락 개발하고 적용

--- README.md
+++ README.md
@@ -4,16 +4,7 @@
 
 
 
-# H2
-
-### 접속 방법
-
-- http://localhost:8080/h2-console
-- jdbc url : jdbc:h2:mem:testdb
-
-
-
-# Redis
+# 프로젝트 시작 방법
 
 
 
@@ -28,7 +19,7 @@ docker-compose -f ./docker-compose-redis.yml up -d
 
 
 
-### 동작 확인
+### redis 동작 확인
 
 - redis-cli 로 접속해서 ping 명령어를 날리면 동작 확인이 가능합니다.
 
@@ -42,8 +33,42 @@ PONT 답변
 
 
 
-### container 종료
+### CoreApplication 실행
 
-```yaml
-docker-compose -f ./docker-compose-redis.yml down
+- 실행하면서 schema.sql 실행되면서 product_order, product 테이블이 생성되며, 데이터가 INSERT 됩니다.
+
+
+
+### 상품 주문 API 호출
+
+- 아래는 userId 1002 에 productId 10002 로 100개를 주문합니다. (이미 데이터가 들어가있습니다.)
+
+```curl
+curl --location --request POST 'localhost:8080/api/v1/orders' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "userId" : "user1002",
+    "productId" : "product1002",
+    "quantity" : 100
+
+}'
 ```
+
+
+
+### 상품 주문 확인 API 호출
+
+- 아래 API 를 호출하면 내가 주문한 내역을 확인할 수 있습니다.
+
+```curl
+curl --location --request GET 'localhost:8080/api/v1/orders' \
+--header 'Content-Type: application/json' \
+--data-raw ''
+```
+
+
+
+`혹시 product 에 대한 재고를 살펴보고 싶으면 H2 접속해서 select 쿼리 날려보면 됩니다.`
+
+- http://localhost:8080/h2-console
+- jdbc url : jdbc:h2:mem:testdb
--- core/core-api/src/main/kotlin/io/simple/productOrder/domain/order/OrderServiceImpl.kt
+++ core/core-api/src/main/kotlin/io/simple/productOrder/domain/order/OrderServiceImpl.kt
@@ -2,6 +2,7 @@ package io.simple.productOrder.domain.order
 
 import io.simple.productOrder.domain.product.ProductCommand
 import io.simple.productOrder.domain.product.ProductExecutor
+import io.simple.productOrder.support.lock.DistributedLock
 import org.mapstruct.factory.Mappers
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -12,28 +13,42 @@ import reactor.core.publisher.Mono
 class OrderServiceImpl(
     private val orderStore: OrderStore,
     private val orderReader: OrderReader,
-    private val productExecutor: ProductExecutor
+    private val productExecutor: ProductExecutor,
+    private val distributedLock: DistributedLock
 ) : OrderService {
 
     @Transactional
     override fun createOrder(createOrder: OrderCommand.CreateOrder): Mono<String> {
-        return productExecutor.reduceStock(
-            ProductCommand.ReduceProduct.of(
-                createOrder.getProductId(),
-                createOrder.getQuantity()
-            )
-        )
-            .flatMap {
-                Mono.just(createOrder)
-                    .map { it.toEntity() }
-                    .flatMap { orderStore.store(it) }
-            }
-            .doOnError { e ->
-                // 예외 처리 로직
+        val lockKey = "product_${createOrder.getProductId()}"
+        return distributedLock.acquireLock(lockKey)
+            .flatMap { lockAcquired ->
+                if (lockAcquired) {
+                    // 락 획득 시, 주문을 생성
+                    productExecutor.reduceStock(
+                        ProductCommand.ReduceProduct.of(
+                            createOrder.getProductId(),
+                            createOrder.getQuantity()
+                        )
+                    )
+                        .flatMap {
+                            Mono.just(createOrder)
+                                .map { it.toEntity() }
+                                .flatMap { orderStore.store(it) }
+                        }
+                        .doFinally {
+                            // 주문 생성 후, 락 해제
+                            distributedLock.releaseLock(lockKey).subscribe()
+                        }
+                } else {
+                    // 락을 획득하지 못한 경우 -> Mono.empty()를 반환
+                    Mono.empty()
+                }
             }
             .onErrorResume { e ->
-                Mono.error(e)
+                // 에러 발생 시, 락 해제
+                distributedLock.releaseLock(lockKey).then(Mono.error(e))
             }
+            .switchIfEmpty(Mono.error(Exception("락을 획득하지 못했습니다.")))
     }
 
     override fun getAllOrders(): Flux<OrderInfo.Base> {
